e88c3561119ea9948275237992ed39cb0b8b7f7f
PalinaBerazavikova <Palina_Berazavikova@epam.com>
2016-09-20 20:47:19 +0300

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Resources;
+using System.Reflection;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int var_first=0;
+            int var_second=0;
+            ResourceManager rm = new ResourceManager("Resourse1", Assembly.GetExecutingAssembly());
+            if (rm.GetString("config").Equals("file")){
+                var_first = Int32.Parse(rm.GetString("val1"));
+                var_second = Int32.Parse(rm.GetString("val2"));
+            }
+            if (rm.GetString("config").Equals("user")){
+                var_first = Int32.Parse(Console.ReadLine());
+                var_second = Int32.Parse(Console.ReadLine());
+            }
+            if (ConfigurationManager.AppSettings["config"].Equals("console"))
+            {
+                Console.WriteLine(var_first + var_second);
+            }
+            if (ConfigurationManager.AppSettings["config"].Equals("class"))
+            {
+                Library.Class1 sol = new Library.Class1();
+                Console.WriteLine(sol.sum(var_first, var_second));
+                Console.WriteLine(sol.subtract(var_first, var_second));
+                Console.WriteLine(sol.multiplicat(var_first, var_second));
+                Console.WriteLine(sol.div(var_first, var_second));
+            }
+            
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Library;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Class1 lib = new Class1();
+            Assert.AreEqual(lib.sum(1, 5), 6);
+            Assert.AreEqual(lib.subtract(1, 5), -4);
+            Assert.AreEqual(lib.multiplicat(1, 5), 5);
+            Assert.AreEqual(lib.div(1, 5), 0.2);
+        }
+    }
+}
